8fc43bc7e57f4d94e888eca79ce0bee10aa64bcc
BataLaSalade <dsfmarina@gmail.com>
2018-06-18 17:10:44 +0200

    Debut des cours de manipulation du DOM

---
+++ Chapitre_1/js/cours.js
@@ -0,0 +1,11 @@
+var h = document.head;
+console.log(h);
+
+var b = document.body;
+console.log(b);
+
+console.log(document.body.childNodes.length);
+
+for (i=0; i<document.body.childNodes.length; i++){
+    console.log(document.body.childNodes[i]);
+}
