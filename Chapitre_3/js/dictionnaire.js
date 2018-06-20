79fd3fb425fdd9b215219ecb15cc4105c81162d1
BataLaSalade <dsfmarina@gmail.com>
2018-06-20 16:33:15 +0200

    re-nommage des variables pour alleger le code

--- Chapitre_3/js/dictionnaire.js
+++ Chapitre_3/js/dictionnaire.js
@@ -15,3 +15,38 @@ var mots = [
 ];
 
 // TODO : créer le dictionnaire sur la page web, dans la div "contenu"
+
+/*function addDico() {
+    for (i=0; i<mots.length; i++){
+        var words = document.createElement("dl");
+        words.id = mots[i].terme;
+        var bold = document.createElement("strong");
+        bold.textContent = mots[i].terme;
+        var termes = document.createElement("dt");
+        termes.id = "terme";
+        var def = document.createElement("dd");
+        def.id = "def";
+        def.textContent = mots[i].definition;
+        document.getElementById("contenu").appendChild(words);
+        words.appendChild(termes);
+        termes.appendChild(bold);
+        words.appendChild(def);
+    };    
+};
+addDico();*/
+function addDico() {
+    for (i=0; i<mots.length; i++){
+        var dlElts = document.createElement("dl");
+        var strongElts = document.createElement("strong");
+        strongElts.textContent = mots[i].terme;
+
+        var dtElts = document.createElement("dt");
+        var ddElts = document.createElement("dd");
+        ddElts.textContent = mots[i].definition;
+        document.getElementById("contenu").appendChild(dlElts);
+        dlElts.appendChild(dtElts);
+        dtElts.appendChild(strongElts);
+        dlElts.appendChild(ddElts);
+    };    
+};
+addDico();
